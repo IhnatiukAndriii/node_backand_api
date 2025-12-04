4810bbecd177fc4a5c6c2f0a452fe37e34e7d1eb
AndriiIhnatiuk <ipz231_iam@student.ztu.edu.ua>
2025-12-04 15:07:56 +0200

    update, fix small bug

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,13 +8,6 @@ const config: { [key: string]: Knex.Config } = {
 			filename: './database.sqlite',
 		},
 		useNullAsDefault: true,
-		pool: {
-			min: 2,
-			max: 10,
-			afterCreate: (conn: any, cb: any) => {
-				conn.run('PRAGMA foreign_keys = ON', cb);
-			},
-		},
 	},
 	test: {
 		client: 'better-sqlite3',
@@ -29,15 +22,6 @@ const config: { [key: string]: Knex.Config } = {
 			filename: process.env.DATABASE_PATH || './database.sqlite',
 		},
 		useNullAsDefault: true,
-		pool: {
-			min: 2,
-			max: 10,
-			acquireTimeoutMillis: 30000,
-			idleTimeoutMillis: 30000,
-			afterCreate: (conn: any, cb: any) => {
-				conn.run('PRAGMA foreign_keys = ON', cb);
-			},
-		},
 	},
 };
 
